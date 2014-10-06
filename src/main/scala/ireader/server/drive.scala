5e402eaa4c1be99163996c03ed180c8695453b9c
John <john@dell>
2014-10-06 19:22:03 +0300

    drafted drive file browser

--- src/main/scala/ireader/server/drive.scala
+++ src/main/scala/ireader/server/drive.scala
@@ -13,16 +13,30 @@ import com.google.api.services.drive.model.About
 class DriveSvlt extends JsonServlet {
     import collection.JavaConversions._
 
-    override def doGet: JValue = {
-        val drive = session.drive.get
-        val children = drive.children.list("root").setQ("trashed = false").execute
-        val result = children.getItems.map { ch => drive.files.get(ch.getId).execute }
-            .map{ f =>
-                ("title" -> f.getTitle) ~
-                ("link" -> f.getAlternateLink)
-                /* ("parents" -> f.getParents.map(_.getId)) */
-            }
+    override def doPost(req: JValue): JValue = {
+        val folder_id: String = req \ "folder_id" match {
+            case JString(s) => s
+            case JNothing => "root"
+            case _ => ???
+        }
 
-        ( "children" -> result )
+        val drive = session.drive.get
+        val children = drive.children.list(folder_id).setQ("trashed = false").execute
+        val resolved = children.getItems.par.map { ch => drive.files.get(ch.getId).execute }
+
+        val (folders, files) = resolved.seq.sortBy(_.getTitle).par.partition(_.getMimeType == DriveSvlt.FOLDER_MIME)
+
+        ("folders" -> folders.map { f=>
+            ("title" -> f.getTitle) ~
+            ("id" -> f.getId)
+        }.seq) ~
+        ("files" -> files.map { f =>
+            ("title" -> f.getTitle) ~
+            ("link" -> f.getAlternateLink)
+        }.seq)
     }
+}
+
+object DriveSvlt {
+    private val FOLDER_MIME = "application/vnd.google-apps.folder"
 }
--- src/main/webapp/drive.coffee
+++ src/main/webapp/drive.coffee
@@ -1,5 +1,14 @@
 window.list_children = (jnode) ->
     $ = window.$
-    $.get 'drive',(data) ->
-        for f in data.children
+    fid = window.location.hash.slice(1)
+    if fid == ''
+        fid = 'root'
+    $.post 'drive',JSON.stringify({folder_id:fid}),(data) ->
+        origin = window.location.href
+        last_hash = origin.lastIndexOf('#')
+        if last_hash >= 0
+            origin = origin.slice(0, last_hash)
+        for f in data.folders
+            jnode.append $("<p/>").append $("<a/>").attr('href', origin + '#' + f.id).text(f.title)
+        for f in data.files
             jnode.append $("<p/>").append $("<a/>").attr('href', f.link).text(f.title)
