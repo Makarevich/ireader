81a9a68f7dc7eeaa7fad9b812d4404c9f86a922d
John <john@dell>
2014-10-07 19:01:05 +0300

    imped batching request and optimized client start-time script

--- src/main/scala/ireader/server/drive.scala
+++ src/main/scala/ireader/server/drive.scala
@@ -1,17 +1,24 @@
 package ireader.server
 
+import concurrent._
+import concurrent.duration._
+
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 
+import com.google.api.client.http.HttpHeaders
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson.JacksonFactory
 import com.google.api.client.auth.oauth2.Credential
-import com.google.api.services.drive.model.About
+import com.google.api.client.googleapis.batch.json.JsonBatchCallback
+import com.google.api.client.googleapis.json.GoogleJsonError
+import com.google.api.services.drive.model.{About, File}
 
 
 
 class DriveSvlt extends JsonServlet {
     import collection.JavaConversions._
+    import ExecutionContext.Implicits.global
 
     override def doPost(req: JValue): JValue = {
         val folder_id: String = req \ "folder_id" match {
@@ -20,20 +27,44 @@ class DriveSvlt extends JsonServlet {
             case _ => ???
         }
 
-        val drive = session.drive.get
-        val children = drive.children.list(folder_id).setQ("trashed = false").execute
-        val resolved = children.getItems.par.map { ch => drive.files.get(ch.getId).execute }
+        val (folders_par, files_par) = {
+            val drive = session.drive.get
+            val children = drive.children.list(folder_id)
+                                         .setQ("trashed = false")
+                                         .execute
 
-        val (folders, files) = resolved.seq.sortBy(_.getTitle).par.partition(_.getMimeType == DriveSvlt.FOLDER_MIME)
+            val resolved_par = {
+                val batch = drive.batch
+                val file_builder = List.newBuilder[File]
+                val batch_callback = new JsonBatchCallback[File] {
+                    def onSuccess(f: File, headers: HttpHeaders) {
+                        file_builder += f
+                    }
+                    def onFailure(err: GoogleJsonError, headers: HttpHeaders): Unit = ???
+                }
 
-        ("folders" -> folders.map { f=>
+                children.getItems.foreach { ch =>
+                    drive.files.get(ch.getId).queue(batch, batch_callback)
+                }
+                batch.execute
+                file_builder.result
+            }
+
+            resolved_par.seq.sortBy(_.getTitle)
+                        .par.partition(_.getMimeType == DriveSvlt.FOLDER_MIME)
+        }
+
+        val folders_json_future = future { folders_par.map { f=>
             ("title" -> f.getTitle) ~
             ("id" -> f.getId)
-        }.seq) ~
-        ("files" -> files.map { f =>
+        }.seq }
+        val files_json_future = future { files_par.map { f =>
             ("title" -> f.getTitle) ~
             ("link" -> f.getAlternateLink)
-        }.seq)
+        }.seq }
+
+        ("folders" -> Await.result(folders_json_future, 30.seconds)) ~
+        ("files" -> Await.result(files_json_future, 30.seconds))
     }
 }
 
--- src/main/webapp/start-time.coffee
+++ src/main/webapp/start-time.coffee
@@ -1,15 +1,22 @@
+issue_time_post = () -> window.jQuery.post("start-time")
+
+repeater = (issue_post, initial_cb, next_cb) ->
+    on_sched = null
+    sched_next = () ->
+        window.setTimeout(on_sched, 1000)
+    on_sched = () ->
+        issue_post().done(sched_next).done(next_cb)
+    issue_post().done(sched_next).done(initial_cb)
+
 window.setup_page_reloader = () ->
     timestamp = ""
-    issue_time_post = () -> window.jQuery.post("start-time")
+
+    on_initial_time = (data) ->
+        timestamp = data
 
     on_new_time = (data) ->
-        if(data != timestamp) 
+        if(data != timestamp)
             console.log("reloading!!")
             window.location.reload(true)
-    schedule_post = () ->
-        window.setTimeout((() -> issue_time_post().done(on_new_time).always(schedule_post)), 1000)
 
-    issue_time_post().done((data) ->
-        timestamp = data
-        schedule_post()
-    )
+    repeater(issue_time_post, on_initial_time, on_new_time)
