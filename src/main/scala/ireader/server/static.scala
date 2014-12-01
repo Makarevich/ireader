3324de6d01fba9ae1489a8626029c81c89722591
John <john@dell>
2014-12-01 16:11:46 +0300

    switched to better routing for html templates

--- src/main/scala/ireader/server/static.scala
+++ src/main/scala/ireader/server/static.scala
@@ -4,11 +4,17 @@ import org.scalatra._
 import scalate.ScalateSupport
 
 class StaticSvlt extends ScalatraServlet with ScalateSupport {
-  notFound {
-    // Try to render a ScalateTemplate if no route matched
-    findTemplate(requestPath) map { path =>
-      contentType = "text/html"
-      layoutTemplate(path)
-    } orElse serveStaticResource() getOrElse resourceNotFound()
-  }
+    get("/") {
+        contentType = "text/html"
+        jade("folder")
+    }
+
+    get("doc") {
+        contentType = "text/html"
+        jade("doc")
+    }
+
+    notFound {
+        serveStaticResource() getOrElse resourceNotFound()
+    }
 }
